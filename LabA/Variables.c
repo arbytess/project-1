79c645dac9db5e6a03016b539cdedeb3390bd5bb
myprcss <preciousvalle1012@gmail.com>
2025-11-15 19:16:50 +0800

    sample code changes

--- LabA/Variables.c
+++ LabA/Variables.c
@@ -11,7 +11,9 @@ int main(void) {
     printf("Age: %d\n", age); // Outputs 18
     printf("GPA: %.2f\n", gpa); // Outputs 1.00
     printf("Name: %s\n", name); // Outputs Arbie
-    printf("Speed: %.2lf km/hr\n", speed); // Outputs 27.5 km/hr
+    printf("Speed: %.2lf km/hr\n", speed); // Outputs 27.5 km/hr 
+
+    printf("sample code");
 
     return 0;
 }
